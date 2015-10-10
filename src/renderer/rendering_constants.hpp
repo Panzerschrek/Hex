7f50059d1c14f2e244e347dcf0a0b3310fba0729
Panzerschrek <AntiPanzerschrek@yandex.ru>
2015-10-10 13:54:09 +0700

    Peredelany objavlenija bazovyh konstant.

--- src/renderer/rendering_constants.hpp
+++ src/renderer/rendering_constants.hpp
@@ -9,5 +9,5 @@ const m_Vec3 R_AMBIENT_LIGHT_COLOR( 0.06f, 0.06f, 0.06f );
 #define R_MIN_TEXTURE_RESOLUTION 4
 
 //z coord of water vertices multiplied by this coeff
-#define R_WATER_VERTICES_Z_SCALER 128
 #define R_WATER_VERTICES_Z_SCALER_LOG2 7
+#define R_WATER_VERTICES_Z_SCALER (1 << R_WATER_VERTICES_Z_SCALER_LOG2)
